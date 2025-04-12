a142655268f673d9e91475e44c9a77a693f13dda
Yashwanth <yashwanth7780@gmail.com>
2025-04-12 20:14:25 +0530

    Starting Week 04

---
+++ Week02/video01/app.js
@@ -0,0 +1,17 @@
+// var i = 20;
+// console.log(i)
+
+function sum(a, b){
+    return a + b;
+}
+// parseInt(a);
+let ans = sum(2, 3);
+console.log(ans);
+
+
+// ASSIGMENT
+// try to create a promisifired version of:
+//   - setTimeout
+//   - fetch
+//   - fs,readFile
+
